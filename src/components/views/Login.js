6bbfadadb6ce82afee664126425e122a0c792a19
qdai <qing.dai>
2023-03-23 16:37:38 +0100

    login change: comment out unused

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 //import {api, handleError} from 'helpers/api';
 //import User from 'models/User';
 //import {useHistory} from 'react-router-dom';
