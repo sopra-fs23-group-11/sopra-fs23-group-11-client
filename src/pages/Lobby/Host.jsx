04c469a9cfef33c554a0b8069e18337870a4c05a
kalilsub <kalil.subaanburaaleh@uzh.ch>
2023-04-12 15:17:19 +0200

    bug fixes to Lobby
    - working endpoints for each host and join

--- src/pages/Lobby/Host.jsx
+++ src/pages/Lobby/Host.jsx
@@ -5,6 +5,8 @@ import {Spinner, Text} from "@chakra-ui/react";
 
 export default function Host() {
   const [code, setCode] = useState(null)
+  const id = localStorage.getItem("userId")
+  const host ={id}
 
   useEffect(() => {
     // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
@@ -12,17 +14,17 @@ export default function Host() {
       try {
         const response = await api.post(
             "/host",
-            JSON.stringify({ username})
+            JSON.stringify({host})
         )
         // delays continuous execution of an async operation for 1 second.
         // This is just a fake async call, so that the spinner can be displayed
         // feel free to remove it :)
 
         // Get the returned users and update the state.
-        setCode(response.data);
+        setCode(response.data.lobbyCode);
 
         // See here to get more data.
-        console.log(response);
+        // console.log(response.data.lobbyCode);
       } catch (error) {
         console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
         console.error("Details:", error);
@@ -30,7 +32,7 @@ export default function Host() {
       }
     }
     fetchData();
-  }, []);
+  }, [0]);
   return (
     <div>
       <div>Code is being generated...</div>
--- src/pages/Lobby/Join.jsx
+++ src/pages/Lobby/Join.jsx
@@ -1,41 +1,52 @@
-import React, {useState, useEffect} from 'react'
-import {Button, Input} from '@chakra-ui/react'
-import {api, handleError} from "../../helpers/api.js";
+import React, { useState, useEffect } from "react"
+import { Button, Input } from "@chakra-ui/react"
+import { api, handleError } from "../../helpers/api.js"
 
 export default function Join() {
-  const [code, setCode]  = useState("")
-    console.log(code)
+  const [lobbyCode, setLobbyCode] = useState("")
+  console.log(lobbyCode)
+  const id = localStorage.getItem("userId")
+  const joiner = { id }
 
-    function submitCode() {
-        useEffect(() => {
-            // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
-            async function fetchData() {
-                try {
-                     await api.put(
-                        "/join",
-                        JSON.stringify({ username, code})
-                    )
-                    // delays continuous execution of an async operation for 1 second.
-                    // This is just a fake async call, so that the spinner can be displayed
-                    // feel free to remove it :)
+  function submitCode() {
+    useEffect(() => {
+      // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
+      async function fetchData() {
+        try {
+          await api.put("/join", JSON.stringify({ joiner, lobbyCode }))
+          // delays continuous execution of an async operation for 1 second.
+          // This is just a fake async call, so that the spinner can be displayed
+          // feel free to remove it :)
 
-                    // Get the returned users and update the state.
+          // Get the returned users and update the state.
 
-                    // See here to get more data.
+          // See here to get more data.
+        } catch (error) {
+          console.error(
+            `Something went wrong while fetching the users: \n${handleError(
+              error
+            )}`
+          )
+          console.error("Details:", error)
+          alert(
+            "Something went wrong while fetching the users! See the console for details."
+          )
+        }
+      }
+      fetchData()
+    }, [])
+  }
 
-                } catch (error) {
-                    console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
-                    console.error("Details:", error);
-                    alert("Something went wrong while fetching the users! See the console for details.");
-                }
-            }
-            fetchData();
-        }, []);
-    }
-
-    return (
-    <div><div>Enter Roomcode</div>
-      <Input value={code} name="code" onChange={e => setCode(e.target.value)} htmlSize={4} width='auto' />
+  return (
+    <div>
+      <div>Enter Roomcode</div>
+      <Input
+        value={lobbyCode}
+        name="code"
+        onChange={(e) => setLobbyCode(e.target.value)}
+        htmlSize={8}
+        width="auto"
+      />
       <Button onClick={submitCode}>submit code</Button>
     </div>
   )
