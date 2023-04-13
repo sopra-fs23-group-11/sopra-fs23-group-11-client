0c2413ab3b93148f7c57b264aef383937d1e0665
Nickschlaedde <nicknicola.schlatter@uzh.ch>
2023-04-13 13:53:58 +0200

    Started on Main Game screen. Created Own Board. Created Submit Shot Button "close #18"

---
+++ src/pages/Play/Play.jsx
@@ -0,0 +1,14 @@
+import MyField from "./MyField.jsx"
+import SubmitButton from "./SubmitButton.jsx"
+import React from "react";
+
+
+function Play() {
+
+    return (
+        <div><MyField />
+        <SubmitButton /></div>
+
+    );
+}
+export default Play;
