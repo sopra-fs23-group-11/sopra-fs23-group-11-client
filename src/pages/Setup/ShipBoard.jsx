5be0bf5fba800f479348f1dc12529a90fe8d905d
qing-dai <qing.dai@uzh.ch>
2023-04-16 00:14:19 +0200

    Show correct ship orientation. #46

--- src/pages/Setup/ShipBoard.jsx
+++ src/pages/Setup/ShipBoard.jsx
@@ -14,6 +14,8 @@ const ShipBoard = (props)=> {
     const[endPointCruiser, setEndPointCruiser] = useState('')
     const[endPointDestroyer, setEndPointDestroyer] = useState('')
     const[endPointSubmarine, setEndPointSubmarine] = useState('')
+    let positions = {};
+
 
     const handleStartPoint = (event, shipType) => {
         const inputValue = event.target.value;
@@ -27,6 +29,22 @@ const ShipBoard = (props)=> {
             if (shipType === 'Destroyer'){setStartPointDestroyer(inputValue)}
             if (shipType === 'Submarine'){setStartPointSubmarine(inputValue)}
         }
+    }
+
+   const handlePlace = () => {
+        const positions = {};
+        if(startPointBattle && endPointBattle){
+            positions['battleShip'] = [startPointBattle, endPointBattle];
+        } if (startPointCarrier && endPointCarrier){
+            positions['carrier'] = [startPointCarrier, endPointCarrier]
+        } if (startPointCruiser && endPointCruiser){
+            positions['cruiser'] = [ startPointCruiser, endPointCruiser]
+        } if (startPointDestroyer && endPointDestroyer){
+            positions['destroyer']=[startPointDestroyer, endPointDestroyer]
+        } if (startPointSubmarine && endPointDestroyer){
+            positions['submarine']=[startPointSubmarine, endPointSubmarine]
+        }
+        props.onShipPlacement(positions)
     }
 
     const handleSubmit = () => {
@@ -53,60 +71,142 @@ const ShipBoard = (props)=> {
         let isHorizontal = false;
         const inputValue = event.target.value;
         console.log(inputValue)
+        console.log('pp1', positions)
         const regex = /^[A-J]\d$/;
-        if(!regex.test(inputValue)){
+        if (!regex.test(inputValue)) {
             alert("Input value must start with a letter from A to J followed by a number from 0 to 9")
         }
-        if(shipType === 'Battleship' && startPointBattle ===''){alert(`For ${shipType}, you should have input in StartPoint first!`)}
-        if(shipType === 'Carrier' && startPointCarrier ===''){alert(`For ${shipType}, you should have input in StartPoint first!`)}
-        if(shipType === 'Cruiser' && startPointCruiser ===''){alert(`For ${shipType}, you should have input in StartPoint first!`)}
-        if(shipType === 'Destroyer' && startPointDestroyer ===''){alert(`For ${shipType}, you should have input in StartPoint first!`)}
-        if(shipType === 'Submarine' && startPointSubmarine ===''){alert(`For ${shipType}, you should have input in StartPoint first!`)}
-        else{
+        if (shipType === 'Battleship' && startPointBattle === '') {
+            alert(`For ${shipType}, you should have input in StartPoint first!`)
+        }
+        if (shipType === 'Carrier' && startPointCarrier === '') {
+            alert(`For ${shipType}, you should have input in StartPoint first!`)
+        }
+        if (shipType === 'Cruiser' && startPointCruiser === '') {
+            alert(`For ${shipType}, you should have input in StartPoint first!`)
+        }
+        if (shipType === 'Destroyer' && startPointDestroyer === '') {
+            alert(`For ${shipType}, you should have input in StartPoint first!`)
+        }
+        if (shipType === 'Submarine' && startPointSubmarine === '') {
+            alert(`For ${shipType}, you should have input in StartPoint first!`)
+        } else {
             if (shipType === 'Battleship') {
-                if (startPointBattle[0] === inputValue[0]){isVertical = true}
-                if (startPointBattle[1] === inputValue[1]){isHorizontal = true}
+                if (startPointBattle[0] === inputValue[0]) {
+                    isHorizontal = true
+                }
+                if (startPointBattle[1] === inputValue[1]) {
+                    isVertical = true
+                }
                 if (isVertical === false && isHorizontal === false) {
                     alert("You should have either vertical or horizontal ship.")
-                } else if (isVertical && Math.abs((+startPointBattle[1])-(+inputValue[1])) !== 3 ){
-                    alert("Check your input squares!")
-                } else if (isHorizontal && Math.abs((startPointBattle[0].charCodeAt(0)-65)-(inputValue[0].charCodeAt(0)-65))!==3){
-                    alert("Check your input squares!")
+                } else if (isHorizontal && Math.abs((+startPointBattle[1]) - (+inputValue[1])) !== 3) {
+                    alert("You want to put horizontal battleship, but your input squares is not 4!")
+                } else if (isVertical && Math.abs((startPointBattle[0].charCodeAt(0) - 65) - (inputValue[0].charCodeAt(0) - 65)) !== 3) {
+                    alert("You want to put vertical battleship, but your input squares is not 4!")
+                } else {
+                    setEndPointBattle(inputValue);
+                positions['battleShip'] = [startPointBattle, inputValue];
+                if(startPointCarrier && endPointCarrier) {positions['carrier'] = [startPointCarrier, endPointCarrier]}
+                if(startPointCruiser && endPointCruiser){positions['cruiser'] = [ startPointCruiser, endPointCruiser]}
+                if(startPointDestroyer && endPointDestroyer ){positions['destroyer']=[startPointDestroyer, endPointDestroyer]}
+                if(startPointSubmarine && endPointSubmarine ){positions['submarine']=[startPointSubmarine, endPointSubmarine]}
+                props.onShipPlacement(positions);
+                console.log(positions)
                 }
-                else (setEndPointBattle(inputValue));
             }
-            if (shipType === 'Carrier'){
-                if (startPointCarrier[0] === inputValue[0]){isVertical = true}
-                if (startPointCarrier[1] === inputValue[1]){isHorizontal = true}
-                if (isVertical === false && isHorizontal === false)
-                { alert("You should have either vertical or horizontal ship.")}
-                else if (isVertical && Math.abs((+startPointCarrier[1])-(+inputValue[1])) !== 4 ){alert("Check your input squares!")}
-                else if(isHorizontal && Math.abs((startPointCarrier[0].charCodeAt(0)-65)-(inputValue[0].charCodeAt(0)-65))!==5){alert("Check your input squares!")}
-                else (setEndPointCarrier(inputValue))
+            if (shipType === 'Carrier') {
+                if (startPointCarrier[0] === inputValue[0]) {
+                    isHorizontal = true
+                }
+                if (startPointCarrier[1] === inputValue[1]) {
+                    isVertical = true
+                }
+                if (isVertical === false && isHorizontal === false) {
+                    alert("You should have either vertical or horizontal ship.")
+                } else if (isHorizontal && Math.abs((+startPointCarrier[1]) - (+inputValue[1])) !== 4) {
+                    alert("You want to put horizontal carrier, but your input squares are not 5!")
+                } else if (isVertical && Math.abs((startPointCarrier[0].charCodeAt(0) - 65) - (inputValue[0].charCodeAt(0) - 65)) !== 4) {
+                    alert("You want to put vertical carrier, but your input squares are not 5!")
+                } else {
+                    setEndPointCarrier(inputValue);
+                    positions['carrier'] = [startPointCarrier, inputValue]
+                if(startPointBattle  && endPointBattle) {positions['battle'] = [startPointBattle, endPointBattle]}
+                if(startPointCruiser  && endPointCruiser ){positions['cruiser'] = [ startPointCruiser, endPointCruiser]}
+                if(startPointDestroyer && endPointDestroyer ){positions['destroyer']=[startPointDestroyer, endPointDestroyer]}
+                if(startPointSubmarine && endPointSubmarine ){positions['submarine']=[startPointSubmarine, endPointSubmarine]}
+                props.onShipPlacement(positions)
+                }
             }
-            if (shipType === 'Cruiser'){
-                if (startPointCruiser[0] === inputValue[0]){isVertical = true}
-                if (startPointCruiser[1] === inputValue[1]){isHorizontal = true}
-                if (isVertical === false && isHorizontal === false){ alert("You should have either vertical or horizontal ship.")}
-                else if (isVertical && Math.abs((+startPointCruiser[1])-(+inputValue[1])) !== 2 ){alert("Check your input squares!")}
-                else if(isHorizontal&& Math.abs((startPointCruiser[0].charCodeAt(0)-65)-(inputValue[0].charCodeAt(0)-65))!==3){alert("Check your input squares!")}
-                else (setEndPointCruiser(inputValue))
+            if (shipType === 'Cruiser') {
+                if (startPointCruiser[0] === inputValue[0]) {
+                    isHorizontal = true
+                }
+                if (startPointCruiser[1] === inputValue[1]) {
+                    isVertical = true
+                }
+                if (isVertical === false && isHorizontal === false) {
+                    alert("You should have either vertical or horizontal ship.")
+                } else if (isHorizontal && Math.abs((+startPointCruiser[1]) - (+inputValue[1])) !== 2) {
+                    alert("You want to put horizontal cruiser, but your input squares are not 3!")
+                } else if (isVertical && Math.abs((startPointCruiser[0].charCodeAt(0) - 65) - (inputValue[0].charCodeAt(0) - 65)) !== 2) {
+                    alert("You want to put vertical cruiser, but your input squares are not 3!")
+                } else {
+                    setEndPointCruiser(inputValue);
+                    positions['cruiser'] = [startPointCruiser,inputValue];
+                if(startPointCarrier && endPointCarrier){positions['carrier'] = [startPointCarrier, endPointCarrier]}
+                if(startPointBattle&&endPointBattle){positions['battle'] = [ startPointBattle, endPointBattle]}
+                if(startPointDestroyer && endPointDestroyer){positions['destroyer']=[startPointDestroyer, endPointDestroyer]}
+                if(startPointSubmarine&&endPointSubmarine){positions['submarine']=[startPointSubmarine, endPointSubmarine]}
+                props.onShipPlacement(positions)
+                }
             }
-            if (shipType === 'Destroyer'){
-                if (startPointDestroyer[0] === inputValue[0]){isVertical = true}
-                if (startPointDestroyer[1] === inputValue[1]){isHorizontal = true}
-                if (isVertical === false && isHorizontal === false){ alert("You should have either vertical or horizontal ship.")}
-                else if (isVertical && Math.abs((+startPointDestroyer[1])-(+inputValue[1])) !== 1 ){alert("Check your input squares!")}
-                else if(isHorizontal&& Math.abs((startPointDestroyer[0].charCodeAt(0)-65)-(inputValue[0].charCodeAt(0)-65))!==2){alert("Check your input squares!")}
-                else (setEndPointDestroyer(inputValue))
+            if (shipType === 'Destroyer') {
+                if (startPointDestroyer[0] === inputValue[0]) {
+                    isHorizontal = true
+                }
+                if (startPointDestroyer[1] === inputValue[1]) {
+                    isVertical = true
+                }
+                if (isVertical === false && isHorizontal === false) {
+                    alert("You should have either vertical or horizontal ship.")
+                } else if (isHorizontal && Math.abs((+startPointDestroyer[1]) - (+inputValue[1])) !== 1) {
+                    alert("You want to put horizontal destroyer, but your input squares are not 2!")
+                } else if (isVertical && Math.abs((startPointDestroyer[0].charCodeAt(0) - 65) - (inputValue[0].charCodeAt(0) - 65)) !== 1) {
+                    alert("You want to put vertical destroyer, but your input squares are not 2!")
+                } else {
+                    setEndPointDestroyer(inputValue)
+                positions['destroyer'] = [startPointDestroyer, inputValue];
+                if(startPointCarrier&&endPointCarrier){positions['carrier'] = [startPointCarrier, endPointCarrier]}
+                if(startPointCruiser&&endPointCruiser){positions['cruiser'] = [ startPointCruiser, endPointCruiser]}
+                if(startPointBattle && endPointBattle){positions['battle']=[startPointBattle, endPointBattle]}
+                if(startPointSubmarine && endPointSubmarine){positions['submarine']=[startPointSubmarine, endPointSubmarine]}
+                props.onShipPlacement(positions)
+                }
             }
-            if (shipType === 'Submarine'){
-                if (startPointSubmarine[0] === inputValue[0]){isVertical = true}
-                if (startPointSubmarine[1] === inputValue[1]){isHorizontal = true}
-                if (isVertical === false && isHorizontal === false){ alert("You should have either vertical or horizontal ship.")}
-                else if (isVertical && Math.abs((+startPointSubmarine[1])-(+inputValue[1])) !== 2 ){alert("Check your input squares!")}
-                else if(isHorizontal&& Math.abs((startPointSubmarine[0].charCodeAt(0)-65)-(inputValue[0].charCodeAt(0)-65))!==3){alert("Check your input squares!")}
-                else (setEndPointSubmarine(inputValue))
+            if (shipType === 'Submarine') {
+                if (startPointSubmarine[0] === inputValue[0]) {
+                    isHorizontal = true
+                }
+                if (startPointSubmarine[1] === inputValue[1]) {
+                    isVertical = true
+                }
+                if (isVertical === false && isHorizontal === false) {
+                    alert("You should have either vertical or horizontal ship.")
+                } else if (isHorizontal && Math.abs((+startPointSubmarine[1]) - (+inputValue[1])) !== 2) {
+                    alert("You want to put horizontal submarine, but your input squares are not 3!")
+                } else if (isVertical && Math.abs((startPointSubmarine[0].charCodeAt(0) - 65) - (inputValue[0].charCodeAt(0) - 65)) !== 2) {
+                    alert("You want to put vertical submarine, but your input squares are not 3!")
+                } else {
+                    setEndPointSubmarine(inputValue)
+                positions['submarine'] = [startPointSubmarine, inputValue];
+                if(startPointCarrier && endPointCarrier){positions['carrier'] = [startPointCarrier, endPointCarrier]}
+                if(startPointCruiser && endPointCruiser){positions['cruiser'] = [ startPointCruiser, endPointCruiser]}
+                if(startPointDestroyer && endPointDestroyer){positions['destroyer']=[startPointDestroyer, endPointDestroyer]}
+                if(startPointBattle && endPointBattle){positions['battle']=[startPointBattle, endPointBattle]}
+                props.onShipPlacement(positions);
+                console.log('ppd', positions)
+                }
             }
         }
     }
@@ -122,6 +222,7 @@ const ShipBoard = (props)=> {
                     <th>ShipsAllowed</th>
                     <th>StartPoint</th>
                     <th>EndPoint</th>
+                    <th>ShipClear</th>
                 </tr>
                 <tr>
                     <td>BattleShip</td>
@@ -129,6 +230,7 @@ const ShipBoard = (props)=> {
                     <td><center>1</center></td>
                     <td><input type="text" onBlur={(event)=>handleStartPoint(event,'Battleship')}/></td>
                     <td><input type="text" onBlur={(event)=>handleEndPoint(event, 'Battleship')}/></td>
+                    <td><button className="place" onClick={handlePlace}>Remove</button></td>
                 </tr>
                 <tr>
                     <td>Carrier</td>
@@ -136,6 +238,7 @@ const ShipBoard = (props)=> {
                     <td><center>1</center></td>
                     <td><input type="text" onBlur={(event)=>handleStartPoint(event,'Carrier')}/></td>
                     <td><input type="text" onBlur={(event)=>handleEndPoint(event, 'Carrier')}/></td>
+                    <td><button className="place" onClick={handlePlace}>Remove</button></td>
                 </tr>
                 <tr>
                     <td>Cruiser</td>
@@ -143,6 +246,7 @@ const ShipBoard = (props)=> {
                     <td><center>1</center></td>
                     <td><input type="text" onBlur={(event)=>handleStartPoint(event,'Cruiser')}/></td>
                     <td><input type="text" onBlur={(event)=>handleEndPoint(event, 'Cruiser')}/></td>
+                    <td><button className="place" onClick={handlePlace}>Remove</button></td>
                 </tr>
                 <tr>
                     <td>Destroyer</td>
@@ -150,6 +254,7 @@ const ShipBoard = (props)=> {
                     <td><center>1</center></td>
                     <td><input type="text" onBlur={(event)=>handleStartPoint(event,'Destroyer')}/></td>
                     <td><input type="text" onBlur={(event)=>handleEndPoint(event, 'Destroyer')}/></td>
+                    <td><button className="place" onClick={handlePlace}>Remove</button></td>
                 </tr>
                 <tr>
                     <td>Submarine</td>
@@ -157,6 +262,7 @@ const ShipBoard = (props)=> {
                     <td><center>1</center></td>
                     <td><input type="text" onBlur={(event)=>handleStartPoint(event,'Submarine')}/></td>
                     <td><input type="text" onBlur={(event)=>handleEndPoint(event, 'Submarine')}/></td>
+                    <td><button className="place" onClick={handlePlace}>Remove</button></td>
                 </tr>
             </table>
             <button className="button" onClick={handleSubmit}>Submit</button>
