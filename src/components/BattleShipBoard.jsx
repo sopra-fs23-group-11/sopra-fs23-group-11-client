c5c58400f8651c1ae7121e711c9dea60473d1eea
kalilsub <kalil.subaanburaaleh@uzh.ch>
2023-04-20 08:45:54 +0200

    Mocking up main Game
    - created ship entities in frontend for submit handler when placing ships
    - mocked up a board component to see websocket functionality (seems to work)

--- src/components/BattleShipBoard.jsx
+++ src/components/BattleShipBoard.jsx
@@ -1,45 +1,90 @@
 import { Box, Grid, GridItem } from "@chakra-ui/react"
 import { React, useState } from "react"
 
-function BattleshipBoard({socket}) {
-  const [board, setBoard] = useState(Array(10).fill(Array(10).fill(false)))
+function BattleshipBoard({ socket }) {
+  const [board, setBoard] = useState(generateBoard())
 
+  function generateBoard() {
+    const rows = 10
+    const cols = 10
+    const boardArray = []
+
+    for (let i = 0; i < rows; i++) {
+      boardArray[i] = []
+      for (let j = 0; j < cols; j++) {
+        boardArray[i][j] = {
+          id: `${String.fromCharCode(65 + i)}${j + 1}`,
+          isHit: false,
+          occupied: null,
+        }
+      }
+    }
+
+    return boardArray
+  }
 
   const handleClick = (e, rowIndex, colIndex) => {
     //console.log(e.target.id)
-    const position = e.target.id
-
-    socket.send("/app/game-simple", {}, JSON.stringify({position}))
+    const position = board[rowIndex][colIndex].id
+    if(socket) socket.send("/app/game-simple", {}, JSON.stringify({ position }))
 
     const newBoard = board.map((row, rIndex) =>
       row.map((col, cIndex) =>
-        rIndex === rowIndex && cIndex === colIndex ? !col : col
+        rIndex === rowIndex && cIndex === colIndex
+          ? { ...col, isHit: !col.isHit }
+          : col
       )
-    );
-    setBoard(newBoard);
+    )
+    setBoard(newBoard)
   }
 
   return (
     <Grid
-      templateColumns="repeat(10, 20px)"
-      templateRows="repeat(10, 20px)"
+      templateColumns="repeat(11, 30px)"
+      templateRows="repeat(11, 30px)"
       gap={0}
-      marginTop="20px"
+      margin="20px"
     >
-      {board.map((row, rowIndex) =>
-        row.map((col, colIndex) => (
-          <GridItem key={`${rowIndex}-${colIndex}`}>
-            <Box
-              id={`${String.fromCharCode(65 + rowIndex)}${colIndex + 1}`}
-              h="20px"
-              w="20px"
-              border="1px solid gray"
-              bg={board[rowIndex][colIndex] ? "blue" : "white"}
-              onClick={(e) => handleClick(e, rowIndex, colIndex)}
-            />
+      <GridItem />
+
+      {Array.from(Array(10)).map((_, index) => (
+        <GridItem
+          key={`row-label-${index}`}
+          justifyContent="center"
+          alignItems="center"
+          h="30px"
+          w="30px"
+          textAlign="center"
+        >
+          {index + 1}
+        </GridItem>
+      ))}
+      {board.map((row, rowIndex) => (
+        <>
+          <GridItem
+            key={`col-label-${rowIndex}`}
+            justifyContent="center"
+            alignItems="center"
+            h="30px"
+            w="30px"
+            textAlign="center"
+          >
+            {String.fromCharCode(65 + rowIndex)}
           </GridItem>
-        ))
-      )}
+          {row.map((col, colIndex) => (
+            <GridItem key={`${rowIndex}-${colIndex}`}>
+              <Box
+                id={`${String.fromCharCode(65 + rowIndex)}${colIndex + 1}`}
+                h="30px"
+                w="30px"
+                border="1px solid gray"
+                bg={board[rowIndex][colIndex].isHit ? "blue" : "white"}
+                onClick={(e) => handleClick(e, rowIndex, colIndex)}
+              />
+            </GridItem>
+          ))}
+        </>
+      ))}
     </Grid>
   )
 }
--- src/pages/Play/Play.jsx
+++ src/pages/Play/Play.jsx
@@ -1,13 +1,18 @@
 import MyField from "./MyField.jsx"
 import SubmitButton from "./SubmitButton.jsx"
 import React from "react";
+import BattleshipBoard from "../../components/BattleShipBoard.jsx";
+import Ships from "../../components/Ships.jsx";
+import { Box } from "@chakra-ui/react";
 
 
 function Play() {
 
     return (
-        <div><MyField />
-        <SubmitButton /></div>
+        <Box>
+            <BattleshipBoard/>
+            <Ships/>
+        </Box>
 
     );
 }
