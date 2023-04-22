974a33794347acae9c49f9fe0614f830f3d1024e
qing-dai <qing.dai@uzh.ch>
2023-04-22 15:52:31 +0200

    Redirect board with ships to "play" page

---
+++ src/pages/Play/GameBoard.jsx
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react';
+import Board from '../Setup/Board.jsx';
+import {ShipPositionsContext} from "../Setup/ShipPositionsContext.jsx";
+import ShipBoard from "../Setup/ShipBoard.jsx";
+import {Box, Button} from "@chakra-ui/react";
+import {Link} from "react-router-dom";
+
+
+const GameBoard =({shipPositions}) => {
+    return (
+        <>
+        <div><Board shipPositions={shipPositions}/></div>
+        <div><Board shipPositions={shipPositions}/></div>
+        </>
+    )
+}
+export default GameBoard;
