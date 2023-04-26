c31165f00a44c9676aff9f88355735ad3809d285
qing-dai <qing.dai@uzh.ch>
2023-04-26 15:40:05 +0200

    Add backrgound battleship img to lobby #69

--- src/pages/Lobby/LobbyLayout.jsx
+++ src/pages/Lobby/LobbyLayout.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import backgroundImage from './battleship.jpeg';
 import { Heading, Box } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
-import backgroundImage from './battleship.jpeg';
+
 
 export default function LobbyLayout() {
   const bgStyle = {
