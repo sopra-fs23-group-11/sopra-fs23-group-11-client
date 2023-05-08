9aa6e454d82d8a0df485983ec1ecc771d783e597
Louis Zürcher <louis.zuercher2@uzh.ch>
2023-05-08 14:58:28 +0200

    patch to fix host

--- src/pages/Lobby/Lobby.jsx
+++ src/pages/Lobby/Lobby.jsx
@@ -1,58 +1,66 @@
 import { Text, Button, Flex } from "@chakra-ui/react"
 
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 
 import { Link } from "react-router-dom"
 import { GameContext } from "../../contexts/GameContext"
+import {api} from "../../helpers/api.js";
 function Lobby() {
   const userData = JSON.parse(sessionStorage.getItem("user"))
-  const { setUser, user } = useContext(GameContext)
+  const [user, setUser] = useState(null);
 
-  // useEffect(() => {
-  //   //once the user is in the lobby we extract the userData from the sessionStorage and store it in context
-  //   setUser((prev) => ({
-  //     ...prev,
-  //     name: userData.username,
-  //     id: userData.id,
-  //     avatar: userData.avatar,
-  //   }))
-  // }, [])
+  useEffect(() => {
+        async function host() {
+          try {
+            const id = userData.id
+            const response = await api.get('/users/' + id)
+            setUser(response.data)
+          } catch (error) {
+            console.log(error.message)
+          }
+        }
 
-  return (
-    <>
-      <Flex
-        flexDirection="column"
-        height="400px"
-        gap="20px"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <h2>
-          <Text fontWeight="bold" color="white">
-            ID: {user.id}
-          </Text>
-        </h2>
-        <h2>
-          <Text fontWeight="bold" color="white">
-            Name: {user.name}
-          </Text>
-        </h2>
-        <Button w="200px" as={Link} to="host" size="lg">
-          Host
-        </Button>
+        host();
+      }
+      , []);
 
-        <Link to="join">
-          <Button w="200px" size="lg">
-            Join
-          </Button>
-        </Link>
+  if (user) {
+    return (
+        <>
+          <Flex
+              flexDirection="column"
+              height="400px"
+              gap="20px"
+              justifyContent="center"
+              alignItems="center"
+          >
+            <h2>
+              <Text fontWeight="bold" color="white">
+                ID: {user.id}
+              </Text>
+            </h2>
+            <h2>
+              <Text fontWeight="bold" color="white">
+                Name: {user.username}
+              </Text>
+            </h2>
+            <Button w="200px" as={Link} to="host" size="lg">
+              Host
+            </Button>
 
-        <Button as={Link} to={`/profile/${user.id}`} w="200px" size="lg">
-          Profile
-        </Button>
-      </Flex>
-    </>
-  )
+            <Link to="join">
+              <Button w="200px" size="lg">
+                Join
+              </Button>
+            </Link>
+
+            <Button as={Link} to={`/profile/${user.id}`} w="200px" size="lg">
+              Profile
+            </Button>
+          </Flex>
+        </>
+    )
+  }
 }
 
 export default Lobby
