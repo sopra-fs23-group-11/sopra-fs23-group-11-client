51f2fb3ca29430783a888c2da481a0a967e01ddd
qdai <qing.dai>
2023-05-09 13:01:44 +0200

    Add background image to each ship and adjust ship box length #87

--- src/components/Ship.jsx
+++ src/components/Ship.jsx
@@ -1,24 +1,38 @@
 import React from "react"
 import { Grid, GridItem, Box } from "@chakra-ui/react"
+import Destroyer from "../models/Destroyer.jpeg"
+import Submarine from "../models/Submarine.jpeg"
+import Cruiser from "../models/Cruiser.jpeg"
+import Carrier from "../models/Carrier.jpeg"
+import Battleship from "../models/Battleship.jpeg"
 
 export default function Ships(props) {
 
    
   return (
-    <Grid templateColumns="repeat(5, 1fr)" gap={4}>
-      <GridItem colSpan={1}>
         <Box 
         borderWidth="1px"
         borderRadius="md"
         p={2}
         textAlign="center"
         onClick={() => props.handleSelect(props.shipId)}
-        bg={props.isHeld  ? "#59E391" : "white"}
-        >
-          <p>{props.type}</p>
-          <p>{props.length}</p>
-        </Box>
-      </GridItem>
-    </Grid>
+        bg={props.isHeld  ? "#59E391" : "#F0F4F8"}
+        opacity = {props.isHeld ? 1 : 0.5}
+        backgroundImage = {
+            props.type === "Destroyer" ? `url(${Destroyer})` :
+            props.type === "Submarine" ? `url(${Submarine})`:
+            props.type === "Cruiser" ? `url(${Cruiser})`:
+            props.type === "BattleShip" ? `url(${Carrier})`:
+            props.type === "Carrier" ? `url(${Battleship})`:null
+        }
+        backgroundSize = "100%"
+        height = "40px"
+        backgroundRepeat = "no-repeat"
+        backgroundPosition = "center"
+        borderBottomWidth = "4px"
+        borderBottomStyle = "solid"
+        borderBottomColor = "#3C4E5A"
+        width={`calc(50px * ${props.length})`}
+        />
   )
 }
